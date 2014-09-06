77a0d863eb1f33dc0ed7c387210155e4412429c9
dydrich <dydrich@yahoo.com>
2014-09-06 12:53:30 +0200

    migliorie grafiche

--- navigation.php
+++ navigation.php
@@ -1,6 +1,6 @@
 <nav id="navigation">
 	<div id="head_label"><p style="margin-top: 5px; vertical-align: top"><?php echo $navigation_label ?></p></div>
-	<div class="nav_div"><a href="<?php echo $_SESSION['__modules__']['docs']['path_to_root'] ?>intranet/<?php echo $_SESSION['__mod_area__'] ?>/index.php" style="position: relative; top: 5px">Home</a></div>
-	<div class="nav_div"><a href="<?php echo $_SESSION['__modules__']['docs']['path_to_root'] ?>intranet/<?php echo $_SESSION['__mod_area__'] ?>/profile.php" style="position: relative; top: 5px">Profilo</a></div>
-	<div class="nav_div"><a href="index.php" style="position: relative; top: 5px">Documenti</a></div>
+	<div class="nav_div"><a href="<?php echo $_SESSION['__modules__']['docs']['path_to_root'] ?>intranet/<?php echo $_SESSION['__mod_area__'] ?>/index.php">&middot; Home</a></div>
+	<div class="nav_div"><a href="<?php echo $_SESSION['__modules__']['docs']['path_to_root'] ?>intranet/<?php echo $_SESSION['__mod_area__'] ?>/profile.php">&middot; Profilo</a></div>
+	<!-- <div class="nav_div"><a href="index.php" style="position: relative; top: 5px">Documenti</a></div> -->
 </nav>
